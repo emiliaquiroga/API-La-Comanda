f03c5ec480ef89f53faa281b2f1a2c3456750996
emiliaquiroga <emiliaquiroga46@gmail.com>
2024-07-08 11:55:14 -0300

    index agrupado para mejor legibilidad

--- app/index.php
+++ app/index.php
@@ -28,7 +28,6 @@ use Slim\Routing\RouteContext;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load(); // Load environment variables
 
-// Instantiate App
 $app = AppFactory::create();
 $conexion = ManipularDatos::obtenerInstancia()->obtenerConexion();
 
@@ -36,166 +35,175 @@ $conexion = ManipularDatos::obtenerInstancia()->obtenerConexion();
 $app->addErrorMiddleware(true, true, true);
 $app->addBodyParsingMiddleware();
 
-$app->get('[/]', function (Request $request, Response $response) {    
-$payload = json_encode(array("mensaje" => "Slim Framework 4 PHP"));
-$response->getBody()->write($payload);
-return $response->withHeader('Content-Type', 'application/json');
+
+// Rutas
+$app->get('[/]', function (Request $request, Response $response) {
+    $payload = json_encode(["mensaje" => "Slim Framework 4 PHP"]);
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->post('/CrearUsuario', function (Request $request, Response $response, array $args)  {
-$controlador = new UsuarioController();
-return $controlador->AsignarUsuario($request, $response, $args);
-});
+// Usuario
+$app->group('/usuarios', function (RouteCollectorProxy $group) {
+    $group->post('/crear', function (Request $request, Response $response, array $args) {
+        $controlador = new UsuarioController();
+        return $controlador->AsignarUsuario($request, $response, $args);
+    });
 
-$app->get('/ListarUsuarios', function (Request $request, Response $response, array $args)  {
-$controlador = new UsuarioController();
-return $controlador->listarUsuario($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio');
-
-$app->put('/ModificarUsuario',function (Request $request, Response $response, array $args)  {
-    $controlador = new UsuarioController();
-    return $controlador->ModificarUno($request, $response, $args);
-    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio'); 
-
-$app->delete('/BorrarUsuario', function (Request $request, Response $response, array $args)  {
-    $controlador = new UsuarioController();
-    return $controlador->BorrarUno($request, $response, $args);
+    $group->get('/listar', function (Request $request, Response $response, array $args) {
+        $controlador = new UsuarioController();
+        return $controlador->listarUsuario($request, $response, $args);
     })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio');
 
-$app->post('/CrearProducto', function (Request $request, Response $response, array $args)  {
-$controlador = new ProductosController();
-return $controlador->AsignarProducto($request, $response, $args);
+    $group->put('/modificar', function (Request $request, Response $response, array $args) {
+        $controlador = new UsuarioController();
+        return $controlador->ModificarUno($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio');
+
+    $group->delete('/borrar', function (Request $request, Response $response, array $args) {
+        $controlador = new UsuarioController();
+        return $controlador->BorrarUno($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio');
 });
 
-$app->get('/ListarProductos', function (Request $request, Response $response, array $args) {
-$controlador = new ProductosController();
-return $controlador->listarProducto($request, $response, $args);
+// Productos
+$app->group('/productos', function (RouteCollectorProxy $group) {
+    $group->post('/crear', function (Request $request, Response $response, array $args) {
+        $controlador = new ProductosController();
+        return $controlador->AsignarProducto($request, $response, $args);
+    });
+
+    $group->get('/listar', function (Request $request, Response $response, array $args) {
+        $controlador = new ProductosController();
+        return $controlador->listarProducto($request, $response, $args);
+    });
+
+    $group->post('/cargarCSV', function(Request $request, Response $response, array $args) {
+        $controlador = new ProductosController();
+        return $controlador->cargarArchivosCSV($request, $response, $args);
+    });
+
+    $group->get('/exportarCSV', function(Request $request, Response $response, array $args) {
+        $controlador = new ProductosController();
+        return $controlador->exportarArchivosCSV($request, $response, $args);
+    });
+
+    $group->get('/descargarPDF', function(Request $request, Response $response, array $args) {
+        $controlador = new ProductosController();
+        return $controlador->exportarArchivosPDF($request, $response, $args);
+    });
 });
 
-$app->post('/AbrirMesa', function(Request $request, Response $response, array $args) {
-$controlador = new MesaController();
-return $controlador->AbrirMesa($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo');
+// Pedidos
+$app->group('/pedidos', function (RouteCollectorProxy $group) {
+    $group->post('/alta', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->darAltaPedido($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo');
 
-$app->get('/ListarMesas', function (Request $request, Response $response, array $args) {
-$controlador = new MesaController();
-return $controlador->listarMesas($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+    $group->get('/listar', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->listarPedidos($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocioOMozo');
 
-$app->post('/AltaPedido', function (Request $request, Response $response, array $args) {
-$controlador = new PedidosController();
-return $controlador->darAltaPedido($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo');
+    $group->get('/buscarPorCodigo', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->obtenerTiempoEstimadoMaximo($request, $response, $args);
+    });
 
-$app->get('/ListarPedidos', function (Request $request, Response $response, array $args){
-$controlador = new PedidosController();
-return $controlador->listarPedidos($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocioOMozo'); 
+    $group->get('/pendientes', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->listarPedidosPendientesPorUsuario($request, $response, $args);
+    })->add(new MiddlewareAutenticacion());
 
-$app->get('/BuscarPorCodigo', function(Request $request, Response $response, array $args){
-$controlador = new PedidosController();
-return $controlador->obtenerTiempoEstimadoMaximo($request, $response, $args);
-});
+    $group->get('/pendientesPorTipoDeProducto', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->listarPedidosPendientes($request, $response, $args);
+    })->add(new MiddlewareEstadoDetallePedido());
 
-$app ->get('/ListarPedidosPendientes', function(Request $request, Response $response, array $args){
-$controlador = new PedidosController();
-return $controlador->listarPedidosPendientes($request, $response, $args);
-});
+    $group->get('/listosParaServir', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->listarPedidosListosParaServir($request, $response);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocioOMozo')->add(new MiddlewareAutenticacion());
 
-$app->get('/pedidos/pendientes', function(Request $request, Response $response, array $args){
-    $controlador = new PedidosController();
-    return $controlador->listarPedidosPendientesPorUsuario($request, $response, $args);
-})->add(new MiddlewareAutenticacion()); 
-
-$app->get('/ListarPendientePorTipoDeProducto', function(Request $request, Response $response, array $args){
-    $controlador = new PedidosController();
-    return $controlador->listarPedidosPendientes($request, $response, $args);
-})->add(new MiddlewareEstadoDetallePedido());   //-----------------------------------chequear mañana
-
-$app->get('/ListarPedidosListosParaServir', function (Request $request, Response $response, array $args){
-    $controlador = new PedidosController();
-    return $controlador->listarPedidosListosParaServir($request, $response);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocioOMozo')->add(new MiddlewareAutenticacion());
-
-
-$app->put('/ModificarEstadoMesa', function (Request $request, Response $response, array $args) {
-$controlador = new MesaController();
-return $controlador->modificarEstadoMesa($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo')->add(new MiddlewareEstadoMesa())->add(new MiddlewareAutenticacion());
-
-$app->put('/ModificarDetallePedido', function (Request $request, Response $response, array $args) {
-$controlador = new PedidosController();
-return $controlador->modificarDetallePedido($request, $response, $args);
-})->add(new MiddlewareEstadoDetallePedido())->add(new MiddlewareAutenticacion());
-
-$app->put('/ModificarEstadoPedido', function (Request $request, Response $response, array $args) {
-    $controlador = new PedidosController();
-    return $controlador->modificarEstadoPedidoGeneral($request, $response, $args);
+    $group->put('/modificarDetalle', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->modificarDetallePedido($request, $response, $args);
     })->add(new MiddlewareEstadoDetallePedido())->add(new MiddlewareAutenticacion());
 
-$app->post('/cobrarCuenta', function (Request $request, Response $response, array $args){
-    $controlador = new PedidosController();
-    return $controlador->cobrarCuenta($request, $response);
-})->add(\MiddlewareVerificarTipoUsuario::class.':ValidarMozo')->add(new MiddlewareAutenticacion()); 
+    $group->put('/modificarEstado', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->modificarEstadoPedidoGeneral($request, $response, $args);
+    })->add(new MiddlewareEstadoDetallePedido())->add(new MiddlewareAutenticacion());
 
-$app->put('/cerrarMesa', function (Request $request, Response $response, array $args){
-    $controlador = new MesaController();
-    return $controlador->modificarEstadoMesa($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+    $group->post('/cobrarCuenta', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->cobrarCuenta($request, $response);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo')->add(new MiddlewareAutenticacion());
 
-
-$app->post('/CargarEncuesta', function (Request $request, Response $response, array $args){
-    $controlador = new EncuestaController();
-    return $controlador->Cargar($request, $response, $args);
+    $group->get('/estadisticas30Dias', function (Request $request, Response $response, array $args) {
+        $controlador = new PedidosController();
+        return $controlador->PromedioIngresos30Dias($request, $response, $args);
+    });
 });
 
-$app->get('/Encuesta/MejoresComentarios', function (Request $request, Response $response, array $args){
-    $controlador = new EncuestaController();
-    return $controlador->TraerMejoresComentarios($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+// Mesas
+$app->group('/mesas', function (RouteCollectorProxy $group) {
+    $group->post('/abrir', function (Request $request, Response $response, array $args) {
+        $controlador = new MesaController();
+        return $controlador->AbrirMesa($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo');
 
-$app->get('/Encuesta/PeoresComentarios', function (Request $request, Response $response, array $args){
-    $controlador = new EncuestaController();
-    return $controlador->TraerPeoresComentarios($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+    $group->get('/listar', function (Request $request, Response $response, array $args) {
+        $controlador = new MesaController();
+        return $controlador->listarMesas($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
 
-$app->get('/Mesas/MasUsada', function (Request $request, Response $response, array $args){
-    $controlador = new MesaController();
-    return $controlador->obtenerMasUsada($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+    $group->put('/modificarEstado', function (Request $request, Response $response, array $args) {
+        $controlador = new MesaController();
+        return $controlador->modificarEstadoMesa($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarMozo')->add(new MiddlewareEstadoMesa())->add(new MiddlewareAutenticacion());
 
-$app->get('/Mesas/MenosUsada', function (Request $request, Response $response, array $args){
-    $controlador = new MesaController();
-    return $controlador->obtenerMenosUsada($request, $response, $args);
-})->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+    $group->put('/cerrar', function (Request $request, Response $response, array $args) {
+        $controlador = new MesaController();
+        return $controlador->modificarEstadoMesa($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
 
+    $group->get('/masUsada', function (Request $request, Response $response, array $args) {
+        $controlador = new MesaController();
+        return $controlador->obtenerMasUsada($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
 
-$app->post('/CargarProductosCSV', function(Request $request, Response $response, array $args){
-    $controlador = new ProductosController();
-    return $controlador->cargarArchivosCSV($request, $response, $args);
-    }); 
-    
-$app->get('/ExportarProductosCSV', function(Request $request, Response $response, array $args){
-    $controlador = new ProductosController();
-    return $controlador->exportarArchivosCSV($request, $response, $args);
-    });
-
-$app->get('/DescargarPDF', function(Request $request, Response $response, array $args){
-    $controlador = new ProductosController();
-    return $controlador->exportarArchivosPDF($request, $response, $args);
-    });
-
-$app->get('/Estadisticas30Dias', function (Request $request, Response $response, array $args){
-    $controlador = new PedidosController();
-    return $controlador->PromedioIngresos30Dias($request, $response, $args);
+    $group->get('/menosUsada', function (Request $request, Response $response, array $args) {
+        $controlador = new MesaController();
+        return $controlador->obtenerMenosUsada($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
 });
 
+// Encuesta
+$app->group('/encuestas', function (RouteCollectorProxy $group) {
+    $group->post('/cargar', function (Request $request, Response $response, array $args) {
+        $controlador = new EncuestaController();
+        return $controlador->Cargar($request, $response, $args);
+    });
+
+    $group->get('/mejoresComentarios', function (Request $request, Response $response, array $args) {
+        $controlador = new EncuestaController();
+        return $controlador->TraerMejoresComentarios($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+
+    $group->get('/peoresComentarios', function (Request $request, Response $response, array $args) {
+        $controlador = new EncuestaController();
+        return $controlador->TraerPeoresComentarios($request, $response, $args);
+    })->add(\MiddlewareVerificarTipoUsuario::class . ':ValidarSocio')->add(new MiddlewareAutenticacion());
+});
+
+//LogIn
 $app->group('/auth', function (RouteCollectorProxy $group) {
     $group->post('/login', function (Request $request, Response $response) {
         $controller = new LogInController();
         return $controller->iniciarSesion($request, $response);
     })->add(new MiddlewareLogIn());
 });
-
 
 $app->run();
